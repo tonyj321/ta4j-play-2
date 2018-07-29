68549c1946bfe7687b123eefe76a4fb9956f15a8
Tony Johnson <tonyj@stanford.edu>
2018-07-28 22:16:21 -0700

    Introduce Point class Fix some logic errors

--- src/main/java/com/jaws/ta4j/play/Trendline.java
+++ src/main/java/com/jaws/ta4j/play/Trendline.java
@@ -1,36 +1,57 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.jaws.ta4j.play;
-
-import java.util.ArrayList;
-import java.util.List;
-import org.ta4j.core.Decimal;
-import org.ta4j.core.Indicator;
 
 /**
  *
  * @author arujohn
  */
-
 public class Trendline {
-    double slope = 0;
-    int startIndex = 0; 
-    double startPrice = 0;
-    public Trendline(double slope, int startIndex, double startPrice){
+
+    private final double slope;
+    private final int startIndex;
+    private final double startPrice;
+
+    public Trendline(double slope, int startIndex, double startPrice) {
         this.slope = slope;
         this.startIndex = startIndex;
         this.startPrice = startPrice;
     }
-    public double getPrice(int index){
+
+    /**
+     * Create a line joining two points
+     * @param index1
+     * @param doubleValue1
+     * @param index2
+     * @param doubleValue2 
+     */
+    Trendline(Integer index1, double doubleValue1, Integer index2, double doubleValue2) {
+        this.slope = (doubleValue2 - doubleValue1) / (index2 - index1);
+        this.startIndex = index1;
+        this.startPrice = doubleValue1;
+    }
+
+    Trendline(Point p1, Point p2) {
+        this.slope = (p2.getValue() - p1.getValue()) / (p2.getIndex() - p1.getIndex());
+        this.startIndex = p1.getIndex();
+        this.startPrice = p1.getValue();
+    }
+
+    public double getPrice(int index) {
         return startPrice + (slope * (index - startIndex));
     }
-    public int getStartIndex(){
+
+    public int getStartIndex() {
         return startIndex;
     }
-    public double getSlope(){
+
+    public double getSlope() {
         return slope;
+    }
+
+    double distanceFrom(Integer index, double doubleValue) {
+        return doubleValue - getPrice(index);
+    }
+
+    double distanceFrom(Point p) {
+        return p.getValue() - getPrice(p.getIndex());
     }
 }
--- src/main/java/com/jaws/ta4j/play/TrendlineIdentifier.java
+++ src/main/java/com/jaws/ta4j/play/TrendlineIdentifier.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.jaws.ta4j.play;
 
-import static java.nio.file.Files.list;
-import static java.rmi.Naming.list;
 import java.util.ArrayList;
-import static java.util.Collections.list;
 import java.util.List;
 import org.ta4j.core.Decimal;
 import org.ta4j.core.Indicator;
@@ -17,43 +9,49 @@ import org.ta4j.core.Indicator;
  *
  * @author arujohn
  */
-public class TrendlineIdentifier{
-    List<Integer> maximas = new ArrayList<>();
-    List<Integer> minimas = new ArrayList<>();
-    List<Trendline> maximaTrends = new ArrayList<>();
-    List<Trendline> minimaTrends = new ArrayList<>();
-    public TrendlineIdentifier(Indicator<Decimal> minMaxIndicator, Indicator<Decimal> priceIndicator){
-        for(int ind=minMaxIndicator.getTimeSeries().getBeginIndex(); ind<minMaxIndicator.getTimeSeries().getBarCount(); ind++){
-            if (minMaxIndicator.getValue(ind).toDouble() == 1){
-                maximas.add(ind);
-            }
-            else if (minMaxIndicator.getValue(ind).toDouble() == -1){
-                minimas.add(ind);
+public class TrendlineIdentifier {
+
+    private final List<Point> maximas = new ArrayList<>();
+    private final List<Point> minimas = new ArrayList<>();
+    private final List<Trendline> maximaTrends = new ArrayList<>();
+    private final List<Trendline> minimaTrends = new ArrayList<>();
+
+    public TrendlineIdentifier(Indicator<Decimal> minMaxIndicator, Indicator<Decimal> priceIndicator) {
+        for (int ind = minMaxIndicator.getTimeSeries().getBeginIndex(); ind < minMaxIndicator.getTimeSeries().getBarCount(); ind++) {
+            if (minMaxIndicator.getValue(ind).doubleValue() == 1) {
+                maximas.add(new Point(ind, priceIndicator.getValue(ind).doubleValue()));
+            } else if (minMaxIndicator.getValue(ind).doubleValue() == -1) {
+                minimas.add(new Point(ind, priceIndicator.getValue(ind).doubleValue()));
             }
         }
         System.out.println(maximas.size());
-        for(int max=0; max<maximas.size(); max++){
-            for (int max2=max; max2<maximas.size(); max2++){
-                double slope = (priceIndicator.getValue(max2).toDouble() - priceIndicator.getValue(max).toDouble())/(max2-max);
-                Trendline currentLine = new Trendline(slope, maximas.get(max), priceIndicator.getValue(maximas.get(max)).toDouble());
+        for (int max = 0; max < maximas.size(); max++) {
+            Point p1 = maximas.get(max);
+            for (int max2 = max+1; max2 < maximas.size(); max2++) {
+                final Point p2 = maximas.get(max2);
+                Trendline currentLine = new Trendline(p1, p2);
                 double quality = 0;
-                for (int max3=max2; max3<maximas.size(); max3++){
-                    if (currentLine.getPrice(max3) < priceIndicator.getValue(max3).toDouble()*1.02 && currentLine.getPrice(max3) > priceIndicator.getValue(max3).toDouble()*0.98){
-                        quality+=1;
+                for (int max3 = max2+1; max3 < maximas.size(); max3++) {
+                    final Point p3 = maximas.get(max3);
+                    double distance = currentLine.distanceFrom(p3);
+                    if (Math.abs(distance/p3.getValue())<0.01) {
+                        quality++;
                     }
                 }
-                double finalProjected = currentLine.getPrice(priceIndicator.getTimeSeries().getEndIndex());
-                double finalPrice = priceIndicator.getTimeSeries().getLastBar().getClosePrice().toDouble();
-                if (quality > 5){
+                //double finalProjected = currentLine.getPrice(priceIndicator.getTimeSeries().getEndIndex());
+                //double finalPrice = priceIndicator.getTimeSeries().getLastBar().getClosePrice().toDouble();
+                if (quality > 4) {
                     maximaTrends.add(currentLine);
                 }
             }
         }
     }
-    public List<Trendline> getMaximaLines(){
+
+    public List<Trendline> getMaximaLines() {
         return maximaTrends;
     }
-    public List<Trendline> getMinimaLines(){
+
+    public List<Trendline> getMinimaLines() {
         return minimaTrends;
     }
 }
